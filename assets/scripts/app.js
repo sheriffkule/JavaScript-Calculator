c4a7cb221c7a372ec8a366db6bb57146ef1cef3c
sheriffkule <zeljko225@yahoo.com>
2024-01-17 22:26:49 +0100

    simple function

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,9 +1,14 @@
 const defaultResult = 0;
-
 let currentResult = defaultResult;
 
-currentResult = (currentResult + 10) * 3 / 2 - 1;
+function add(num1, num2) {
+    const result = num1 + num2;
+    return result;
+}
+
+currentResult = add(2, 3);
 
 let calculationDescription = `(${defaultResult} + 10) * 3 / 2 - 1`;
 
 outputResult(currentResult, calculationDescription);
+
