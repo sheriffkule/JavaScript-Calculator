8d6243af93a47a93c2c54ce21c8741c49159ea98
sheriffkule <zeljko225@yahoo.com>
2024-01-18 10:07:09 +0100

    splitting code into functions

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,13 +1,15 @@
 const defaultResult = 0;
 let currentResult = defaultResult;
 
+function getUserNumberInput() {
+    return parseInt(userInput.value);
+}
+
 function add() {
-    currentResult = currentResult + userInput.value;
-    outputResult(currentResult, '');
+    const enteredNumber = getUserNumberInput;
+    const calcDescription = `${currentResult} + ${userInput.value}`
+    currentResult = currentResult + enteredNumber;
+    outputResult(currentResult, calcDescription);
 }
 
 addBtn.addEventListener('click', add);
-
-// currentResult = add(2, 3);
-
-// let calculationDescription = `(${defaultResult} + 10) * 3 / 2 - 1`;
